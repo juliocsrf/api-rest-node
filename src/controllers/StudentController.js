9abc718673bfa4382cad251a6bf6ae0f11bbbec2
Julio Fonseca <juliocsrmf@gmail.com>
2022-07-27 20:53:05 -0300

    Students crud

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -5,6 +5,99 @@ class StudentController {
         const students = await Student.findAll();
         res.json({ students });
     }
+
+    async store(req, res) {
+        try {
+            const student = await Student.create(req.body);
+
+            return res.json({ student });
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async show(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const student = await Student.findByPk(id);
+
+            if (!student) {
+                return res.status(400).json({
+                    errors: ['Aluno inválido'],
+                });
+            }
+
+            return res.json({ student });
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const student = await Student.findByPk(id);
+
+            if (!student) {
+                return res.status(400).json({
+                    errors: ['Aluno inválido'],
+                });
+            }
+
+            await student.destroy();
+
+            return res.json({});
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (!id) {
+                return res.status(400).json({
+                    errors: ['Faltando ID'],
+                });
+            }
+
+            const student = await Student.findByPk(id);
+
+            if (!student) {
+                return res.status(400).json({
+                    errors: ['Aluno inválido'],
+                });
+            }
+
+            const studentUpdated = await student.update(req.body);
+            return res.json({ student: studentUpdated });
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
 }
 
 export default new StudentController();
--- src/models/Student.js
+++ src/models/Student.js
@@ -3,12 +3,62 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
     static init(sequelize) {
         super.init({
-            name: Sequelize.STRING,
-            lastName: Sequelize.STRING,
-            email: Sequelize.STRING,
-            age: Sequelize.INTEGER,
-            weight: Sequelize.FLOAT,
-            height: Sequelize.FLOAT,
+            name: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    len: {
+                        args: [3, 255],
+                        msg: 'Nome deve ter entre 3 e 255 caracteres',
+                    },
+                },
+            },
+            lastName: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    len: {
+                        args: [3, 255],
+                        msg: 'Sobrenome deve ter entre 3 e 255 caracteres',
+                    },
+                },
+            },
+            email: {
+                type: Sequelize.STRING,
+                defaultValue: '',
+                validate: {
+                    isEmail: {
+                        msg: 'E-mail inválido',
+                    },
+                },
+            },
+            age: {
+                type: Sequelize.INTEGER,
+                defaultValue: '',
+                validate: {
+                    isInt: {
+                        msg: 'A idade deve ser um número inteiro',
+                    },
+                },
+            },
+            weight: {
+                type: Sequelize.FLOAT,
+                defaultValue: '',
+                validate: {
+                    isFloat: {
+                        msg: 'O peso deve ser um número inteiro ou de ponto flutuante',
+                    },
+                },
+            },
+            height: {
+                type: Sequelize.FLOAT,
+                defaultValue: '',
+                validate: {
+                    isFloat: {
+                        msg: 'A altura deve ser um número inteiro ou de ponto flutuante',
+                    },
+                },
+            },
         }, { sequelize });
         return this;
     }
