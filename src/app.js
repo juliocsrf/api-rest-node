262aefcf6110f5063f27c99e10a7aee8f162803c
Julio Fonseca <juliocsrmf@gmail.com>
2022-09-11 01:04:47 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ class App {
     }
 
     middlewares() {
-        this.app.use(cors());
+        this.app.use(cors({ origin: '*' }));
         this.app.use(helmet());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
